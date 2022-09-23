f7c30b9e5159f56408bb8074dafa80a5bb7000f3
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2022-09-24 00:47:57 +0200

    initial struct for spot prices

--- pkg/infra/aws/aws.go
+++ pkg/infra/aws/aws.go
@@ -1,45 +1,70 @@
 package aws
 
+import (
+	"sort"
+	"strconv"
+
+	"github.com/adrianriobo/pupoc/pkg/util"
+	"github.com/adrianriobo/pupoc/pkg/util/logging"
+	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/ec2"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+type StackRequest struct {
+	ProductDescription string
+	InstanceType       string
+	AvailabilityZones  []string
+}
+
 // https://github.com/pulumi/automation-api-examples/blob/f5444239378c9891250ee367e9c2a6f26149f375/go/multi_stack_orchestration/main.go#L132
 
-// this is the inline pulumi function for our s3 bucket stack
-// func websiteFunc(ctx *pulumi.Context) error {
-// 	// similar go git_repo_program, our program defines a s3 website.
-// 	// here we create the bucket
-// 	siteBucket, err := s3.NewBucket(ctx, "s3-website-bucket", &s3.BucketArgs{
-// 		Website: s3.BucketWebsiteArgs{
-// 			IndexDocument: pulumi.String("index.html"),
-// 		},
-// 	})
-// 	if err != nil {
-// 		return err
-// 	}
+func (s StackRequest) BidSpotPrice(ctx *pulumi.Context) error {
+	var spotPrices []*ec2.GetSpotPriceResult
 
-// 	// Set the access policy for the bucket so all objects are readable.
-// 	if _, err := s3.NewBucketPolicy(ctx, "bucketPolicy", &s3.BucketPolicyArgs{
-// 		Bucket: siteBucket.ID(), // refer to the bucket created earlier
-// 		Policy: pulumi.Any(map[string]interface{}{
-// 			"Version": "2012-10-17",
-// 			"Statement": []map[string]interface{}{
-// 				{
-// 					"Effect":    "Allow",
-// 					"Principal": "*",
-// 					"Action": []interface{}{
-// 						"s3:GetObject",
-// 					},
-// 					"Resource": []interface{}{
-// 						pulumi.Sprintf("arn:aws:s3:::%s/*", siteBucket.ID()), // policy refers to bucket name explicitly
-// 					},
-// 				},
-// 			},
-// 		}),
-// 	}); err != nil {
-// 		return err
-// 	}
+	// If empty azs it will check all non opted-in
+	availabilityZones := util.If(len(s.AvailabilityZones) != 0,
+		s.AvailabilityZones,
+		GetNotOptedInAvailabilityZones(ctx))
 
-// 	// export the website URL
-// 	ctx.Export("websiteUrl", siteBucket.WebsiteEndpoint)
-// 	// also export the bucketID for Object stack to refer to
-// 	ctx.Export("bucketID", siteBucket.ID())
-// 	return nil
-// }
+	for _, availabilityZone := range availabilityZones {
+		if spotPrice, err := s.getSpotPrice(availabilityZone, ctx); err != nil {
+			logging.Debugf("Can not get price for %s on %s due to %v", s.InstanceType, availabilityZone, err)
+		} else {
+			spotPrices = append(spotPrices, spotPrice)
+		}
+	}
+	minSpotPrice := minSpotPrice(spotPrices)
+	if minSpotPrice != nil {
+		ctx.Export(StackSpotOutputSpotPrice, pulumi.String(minSpotPrice.SpotPrice))
+		ctx.Export(StackSpotOutputAvailabilityZone, pulumi.String(*minSpotPrice.AvailabilityZone))
+	}
+	// export the website URL
+	return nil
+}
+
+func (s StackRequest) getSpotPrice(az string, ctx *pulumi.Context) (*ec2.GetSpotPriceResult, error) {
+	return ec2.GetSpotPrice(ctx, &ec2.GetSpotPriceArgs{
+		AvailabilityZone: pulumi.StringRef(az),
+		Filters: []ec2.GetSpotPriceFilter{
+			{
+				Name: spotQueryFilterProductDescription,
+				Values: []string{
+					s.ProductDescription,
+				},
+			},
+		},
+		InstanceType: pulumi.StringRef(s.InstanceType),
+	}, nil)
+}
+
+func minSpotPrice(source []*ec2.GetSpotPriceResult) *ec2.GetSpotPriceResult {
+	if len(source) == 0 {
+		return nil
+	}
+	sort.Slice(source, func(i, j int) bool {
+		iPrice, _ := strconv.ParseFloat(source[i].SpotPrice, 64)
+		jPrice, _ := strconv.ParseFloat(source[j].SpotPrice, 64)
+		return iPrice < jPrice
+	})
+	return source[0]
+}
